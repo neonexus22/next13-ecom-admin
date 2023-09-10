e475584b79fd9fe7965bbd8251a082b838dd10ee
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-10 17:48:46 +0545

    checked for one store and pushed to store page

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+type IParams = {
+  children: ReactNode;
+  params: {
+    storeId: string;
+  };
+};
+
+export default async function DashboardLayout({ children, params }: IParams) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div>This will be a Navbar</div>
+      {children}
+    </>
+  );
+}
