e129a2b0ebf6379497462e3816521bc4ccbd4c09
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-15 14:33:07 +0545

    added orders page

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { DataTable } from "@/components/ui/data-table";
-import { Heading } from "@/components/ui/heading";
+import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 import { columns, OrderColumn } from "./columns";
@@ -10,12 +10,13 @@ interface OrderClientProps {
   data: OrderColumn[];
 }
 
-export const OrderClient: React.FC<OrderClientProps> = ({
-  data
-}) => {
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
   return (
     <>
-      <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
       <Separator />
       <DataTable searchKey="products" columns={columns} data={data} />
     </>
