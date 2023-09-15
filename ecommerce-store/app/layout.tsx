488451c7f16c40aab8c012c2af4e4f48077af641
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-15 15:21:31 +0545

    added store initial setup

---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,26 @@
+import Footer from "@/components/footer";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
