9d6f5cb87633a2ad6a1793825d48b9360655a5eb
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-16 17:26:56 +0545

    added info section

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 import React, { FC } from "react";
@@ -24,8 +25,7 @@ const ProductPage: FC<ProductPageProps> = async ({ params }) => {
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-8">
             <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {/* info section */}
-              Info
+              <Info data={product} />
             </div>
           </div>
           <hr className="my-10" />
---
+++ ecommerce-store/components/info.tsx
@@ -0,0 +1,44 @@
+import { Product } from "@/types";
+import React, { FC } from "react";
+import Currency from "@/components/ui/currency";
+import Button from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
+
+type InfoProps = {
+  data: Product;
+};
+
+const Info: FC<InfoProps> = ({ data }) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+      <div className="mt-3 flex items-end justify-between">
+        <p className="text-2xl text-gray-900">
+          <Currency value={data?.price} />
+        </p>
+      </div>
+      <hr className="my-4" />
+      <div className="flex flex-col gap-y-6">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Size:</h3>
+          <div>{data?.size?.name}</div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Color:</h3>
+          <div
+            className="w-4 h-4 rounded-full border border-gray-600"
+            style={{ backgroundColor: data?.color?.value }}
+          />
+        </div>
+      </div>
+      <div className="mt-10 flex items-center gap-x-3">
+        <Button className="flex items-center gap-x-2">
+          Add to cart
+          <ShoppingCart />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
